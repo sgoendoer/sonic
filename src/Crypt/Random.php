a61c71143690a88135dbf01962c9e87acb69bbcc
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-01-23 14:01:47 +0100

    added documentation

--- src/Crypt/Random.php
+++ src/Crypt/Random.php
@@ -11,11 +11,23 @@ use sgoendoer\Sonic\Sonic;
  */
 class Random
 {
+	/**
+	 * creates a random character sequence of length $length
+	 * 
+	 * @param $length integer. Defaults to 8
+	 * @return string
+	 */
 	public static function getRandom($length = 8)
 	{
 		return bin2hex(openssl_random_pseudo_bytes($length));
 	}
 	
+	/**
+	 * creates a random character sequence of length $length. The created value is checked against already used values for this account to guarantee uniquenes. The newly created value is then registered via the UniqueIDManager of the SDK.
+	 * 
+	 * @param $length integer. Defaults to 8
+	 * @return string
+	 */
 	public static function getUniqueRandom($length = 8)
 	{
 		$id = self::getRandom($length);
