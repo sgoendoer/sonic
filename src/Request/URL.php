cb675008a22d13e8208b37d85a4ae8c5d5de391c
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-01-25 10:08:38 +0100

    Moved class URL to Sonic/Request

--- src/Request/URL.php
+++ src/Request/URL.php
@@ -1,27 +1,45 @@
-<?php namespace sgoendoer\Sonic\Tools;
+<?php namespace sgoendoer\Sonic\Request;
 
 /**
  * URL helper class
- * version 20160104
+ * version 20160125
  *
  * author: Sebastian Goendoer
  * copyright: Sebastian Goendoer <sebastian.goendoer@rwth-aachen.de>
  */
 class URL
 {
+	/**
+	 * Extracts the port from a given $url
+	 * 
+	 * @param $url string The URL
+	 * 
+	 * @return integer the port number. If not specified or invalid, the default http port 80 is returned.
+	 */
 	public static function getPortFromURL($url)
 	{
 		$url = self::getDomainFromProfileLocation($url);
 		$url = explode(':', $url);
 		
 		if(count($url) == 1)
+		{
 			return 80;
+		}
 		else
-			return $url[1];
+		{
+			if(!is_numeric($url[1]))
+				return 80;
+			else
+				return (integer) $url[1];
+		}
 	}
 	
 	/**
-	 * extracts the domain from a URL
+	 * Extracts the domain from a URL
+	 * 
+	 * @param $url string The URL
+	 * 
+	 * @return string The domain
 	 */
 	public static function getDomainFromURL($url)
 	{
@@ -29,6 +47,13 @@ class URL
 		return explode('/', $url)[0];
 	}
 	
+	/**
+	 * Extracts the path from a URL
+	 * 
+	 * @param $url string The URL
+	 * 
+	 * @return string The path
+	 */
 	public static function getPathFromURL($url)
 	{
 		$domain = self::getDomainFromProfileLocation($url);
