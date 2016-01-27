bbd0ac4c1b5997c762af0b88a26b7e3f40b8e7b4
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-01-27 09:39:39 +0100

    Added function to build JWT

--- src/Identity/EntityAuthData.php
+++ src/Identity/EntityAuthData.php
@@ -18,7 +18,16 @@ class EntityAuthData
 	private $socialRecord		= NULL;	// socialRecord
 	private $personalKeyPair	= NULL; // 
 	private $accountKeyPair		= NULL; // 
+	private $jwt				= NULL;
 	
+	/**
+	 * Initializes EntityAuthData with the auth data of a user or a platform. While the parameters $socialRecord and
+	 * $accountKeyPair are mandatory, the $personalKeyPair is optional and may be omitted.
+	 * 
+	 * @param $socialRecord SocialRecord The SocialRecord of the entity
+	 * @param $accountKeyPair KeyPair The accountKeyPair for the entity
+	 * @param $personalKeyPair KeyPair The personalKeyPair for the entity. OPTIONAL!
+	 */
 	public function __construct(SocialRecord $socialRecord,
 								KeyPair $accountKeyPair,
 								KeyPair $personalKeyPair = NULL)
@@ -64,7 +73,28 @@ class EntityAuthData
 		$this->accountKeyPair = $accountKeyPair;
 	}
 	
-	// TODO provide functionality to store and create JWT
+	/**
+	 * Creates  a signed JWT to be pushed to the GSLS. If the personalKeyPair is not set, an exception is thrown
+	 * 
+	 * @return String The JWT
+	 */
+	public function getJWT()
+	{
+		if($this->personalKeyPair === NULL)
+			throw new \Exception("JWT cannot be built without the personalKey");
+		
+		// create and sign JWT
+		$signer = new Sha512();
+		
+		$personalPrivateKey = PrivateKey::formatPEM($this->personalKeyPair->getPrivateKey());
+		
+		$token = (new Builder())
+			->set('socialRecord', base64_encode($this->socialRecord->getJSONString()))
+			->sign($signer, $personalPrivateKey)
+			->getToken();
+		
+		return $token->__toString();
+	}
 }
 
 ?>
