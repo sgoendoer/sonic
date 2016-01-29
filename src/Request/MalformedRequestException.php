f8a6ca7f30836e99285be1ad60d8ad36135483e3
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-01-29 13:52:43 +0100

    added malformedRequestException

---
+++ src/Request/MalformedRequestException.php
@@ -0,0 +1,17 @@
+<?php namespace sgoendoer\Sonic\Request;
+
+use sgoendoer\Sonic\Sonic;
+use sgoendoer\Sonic\Config\Configuration;
+
+class MalformedRequestException extends \Exception
+{
+	public function __construct($message = null, $code = 0, \Exception $previous = null)
+	{
+		parent::__construct($message, $code, $previous);
+		
+		if(Configuration::getVerbose() >= 1)
+			Sonic::getLogger()->addError($message);
+	}
+}
+
+?>
