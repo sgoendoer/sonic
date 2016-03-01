801e60f2c12f6773f45b6b104c138c7d8e30ec60
SenanSharhan <senan.sharhan@hotmail.com>
2016-03-01 17:46:08 +0100

    add Delete user

--- src/Api/MigrationRequestBuilder.php
+++ src/Api/MigrationRequestBuilder.php
@@ -27,5 +27,16 @@ class MigrationRequestBuilder extends AbstractRequestBuilder
         return $this;
     }
 
+    public function createDELETEUser($authToken)
+    {
+        $this->request = new OutgoingRequest();
+        $this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
+        $this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_MIGRATION);
+        $this->request->setRequestMethod('DELETE');
+        $this->request->setHeaderAuthToken($authToken);
+
+        return $this;
+    }
+
 
 }
