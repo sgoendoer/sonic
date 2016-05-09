8a8465df4ec50cf379c9051f8e5ed4f483455428
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-05-09 11:38:00 +0200

    default HTTP response code and message are now "200 OK" added phpdoc added MalformedResponseException being thrown on verification failure added MalformedResponseException class

--- src/Request/OutgoingResponse.php
+++ src/Request/OutgoingResponse.php
@@ -5,16 +5,20 @@ use sgoendoer\Sonic\Crypt\Random;
 use sgoendoer\Sonic\Crypt\Signature;
 use sgoendoer\Sonic\Date\XSDDateTime;
 use sgoendoer\Sonic\Request\AbstractRequest;
+use sgoendoer\Sonic\Request\MalformedResponseException;
 
 /**
  * OutgoingResponse
- * version 20160111
+ * version 20160509
  *
  * author: Sebastian Goendoer
  * copyright: Sebastian Goendoer <sebastian.goendoer@rwth-aachen.de>
  */
 class OutgoingResponse extends AbstractResponse
 {
+	/**
+	 * constructor for building a Sonic compliant HTTP response.
+	 */
 	public function __construct()
 	{
 		$this->headers = array();
@@ -23,12 +27,20 @@ class OutgoingResponse extends AbstractResponse
 		$this->headers[SONIC_HEADER__TARGET_API]	= SONIC_SDK__API_VERSION;
 		$this->headers[SONIC_HEADER__PLATFORM_GID]	= Sonic::getPlatformGlobalID();
 		$this->headers[SONIC_HEADER__SOURCE_GID]	= Sonic::getContextGlobalID();
+		
+		$this->statusCode = 200;
+		$this->statusMessage = 'OK';
 	}
 	
+	/**
+	 * sends the HTTP response
+	 * 
+	 * @throws MalformesResponseException if the response is invalid
+	 */
 	public function send()
 	{
 		if(!$this->verify())
-			throw new \Exception("Malformed response");
+			throw new \MalformesResponseException("Malformed response");
 		else
 		{
 			http_response_code($this->statusCode);
@@ -42,36 +54,57 @@ class OutgoingResponse extends AbstractResponse
 		echo $this->body;
 	}
 	
+	/**
+	 * signs the response object
+	 */
 	public function signResponse($privateAccountKey)
 	{
 		$this->headers[SONIC_HEADER__SIGNATURE] = Signature::createSignature($this->getStringForResponseSignature(), $privateAccountKey);
 	}
 	
+	/**
+	 * sets the HTTP status code
+	 */
 	public function setResponseStatusCode($statusCode)
 	{
 		$this->statusCode = $statusCode;
 	}
 	
+	/**
+	 * sets the response's body
+	 */
 	public function setResponseBody($body)
 	{
 		$this->body = $body;
 	}
 	
+	/**
+	 * sets the SonicPlatformGID header
+	 */
 	public function setHeaderPlatformGID($gid)
 	{
 		$this->headers[SONIC_HEADER__PLATFORM_GID] = $gid;
 	}
 	
+	/**
+	 * sets the SonicSourceGID header
+	 */
 	public function setHeaderSourceGID($gid)
 	{
 		$this->headers[SONIC_HEADER__SOURCE_GID] = $gid;
 	}
 	
+	/**
+	 * sets the SonicDate header
+	 */
 	public function setHeaderDate($date)
 	{
 		$this->headers[SONIC_HEADER__DATE] = $date;
 	}
 	
+	/**
+	 * sets the SonicTargetAPI header
+	 */
 	public function setHeaderTargetAPI($api)
 	{
 		$this->headers[SONIC_HEADER__TARGET_API] = $api;
