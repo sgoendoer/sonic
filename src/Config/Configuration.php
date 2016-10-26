b38122c18ff2b1670a0f34bf47b123fad2a9ad3b
SebastianGoendoer <sgoendoer@users.noreply.github.com>
2016-10-26 16:56:08 +0200

    PHPdoc

--- src/Config/Configuration.php
+++ src/Config/Configuration.php
@@ -2,10 +2,10 @@
 
 /**
  * Configuration
- * version 20160201
+ * @version 20160201
  *
- * author: Sebastian Goendoer
- * copyright: Sebastian Goendoer <sebastian [dot] goendoer [at] gmail [dot] com>
+ * @author Sebastian Goendoer
+ * @copyright Sebastian Goendoer <sebastian [dot] goendoer [at] gmail [dot] com>
  */
 class Configuration
 {
@@ -22,6 +22,11 @@ class Configuration
 	private function __construct() {}
 	private function __clone() {}
 	
+	/**
+	 * Sets configuration values
+	 * 
+	 * @param $config Array The configuration value
+	 */
 	public static function setConfiguration($config)
 	{
 		if(!is_array($config))
@@ -39,91 +44,163 @@ class Configuration
 		if(array_key_exists('logfile')) self::$logfile = $config['logfile'];
 	}
 	
+	/**
+	 * returns the primary GSLS node address
+	 */
 	public static function getPrimaryGSLSNode()
 	{
 		return self::$primaryGSLSNode;
 	}
 	
+	/**
+	 * Sets the primary GSLS node's IP address
+	 * 
+	 * @param $ipAddress String The IP Address
+	 */
 	public static function setPrimaryGSLSNode($ipAddress)
 	{
 		self::$primaryGSLSNode = $ipAddress;
 	}
 	
+	/**
+	 * returns the secondary GSLS node address
+	 */
 	public static function getSecondaryGSLSNode()
 	{
 		return self::$secondaryGSLSNode;
 	}
 	
+	/**
+	 * Sets the secondary GSLS node's IP address
+	 * 
+	 * @param $ipAddress String The IP Address
+	 */
 	public static function setSecondaryGSLSNode($ipAddress)
 	{
 		self::$secondaryGSLSNode = $ipAddress;
 	}
 	
+	/**
+	 * returns the API path
+	 */
 	public static function getApiPath()
 	{
 		return self::$apiPath;
 	}
 	
+	/**
+	 * Sets the API path
+	 * 
+	 * @param $apiPath String The API path
+	 */
 	public static function setApiPath($path)
 	{
 		self::$apiPath = $path;
 	}
 	
+	/**
+	 * returns the timezone
+	 */
 	public static function getTimezone()
 	{
 		return self::$timezone;
 	}
 	
+	/**
+	 * Sets the timezone
+	 * 
+	 * @param $tz String The timezone
+	 */
 	public static function setTimezone($tz)
 	{
 		self::$timezone = $tz;
 	}
 	
+	/**
+	 * returns the verbose value
+	 */
 	public static function getVerbose()
 	{
 		return self::$verbose;
 	}
 	
+	/**
+	 * Sets the verbose value
+	 * 
+	 * @param $verbose int The verbose value
+	 */
 	public static function setVerbose($verbose)
 	{
 		self::$verbose = $verbose;
 	}
 	
+	/**
+	 * returns the CURL verbose value
+	 */
 	public static function getCurlVerbose()
 	{
 		return self::$curlVerbose;
 	}
 	
+	/**
+	 * Sets the CURL verbose value
+	 * 
+	 * @param $curlVerbose int The CURL verbose value
+	 */
 	public static function setCurlVerbose($curlVerbose)
 	{
 		self::$curlVerbose = $curlVerbose;
 	}
 	
+	/**
+	 * returns the request timeout value
+	 */
 	public static function getRequestTimeout()
 	{
 		return self::$requestTimeout;
 	}
 	
+	/**
+	 * Sets the request timeout
+	 * 
+	 * @param $requestTimeout int The request timeout
+	 */
 	public static function setRequestTimeout($requestTimeout)
 	{
 		self::$requestTimeout = $requestTimeout;
 	}
 	
+	/**
+	 * returns the GSLS timeout value
+	 */
 	public static function getGSLSTimeout()
 	{
 		return self::$gslsTimeout;
 	}
 	
+	/**
+	 * Sets the GSLS timeout
+	 * 
+	 * @param $gslsTimeout int The GSLS timeout
+	 */
 	public static function setGSLSTimeout($gslsTimeout)
 	{
 		self::$gslsTimeout = $gslsTimeout;
 	}
 	
+	/**
+	 * returns the logfile path
+	 */
 	public static function getLogfile()
 	{
 		return self::$logfile;
 	}
 	
+	/**
+	 * Sets the logfile path
+	 * 
+	 * @param $logfile String The logfile path
+	 */
 	public static function setLogfile($logfile)
 	{
 		self::$logfile = $logfile;
