345f7381ec949e3be006a26a5a9bf7c7f61d9cf7
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2016-11-17 15:17:25 +0000

    v 0.3.0 version codes

--- src/init.inc.php
+++ src/init.inc.php
@@ -31,8 +31,8 @@ define('SONIC_HEADER__RANDOM',			'SonicRandom');
 define('SONIC_HEADER__AUTH_TOKEN',		'SonicAuthToken');
 
 define('SONIC_SDK__APP_NAME',			'SonicSDK');
-define('SONIC_SDK__APP_VERSION',		'0.2.5');
-define('SONIC_SDK__APP_VERSION_NAME',	'beta2.5');
+define('SONIC_SDK__APP_VERSION',		'0.3.0');
+define('SONIC_SDK__APP_VERSION_NAME',	'beta3.0');
 define('SONIC_SDK__API_VERSION',		'0.1.2');
 
 define('SONIC_REQUEST__USERAGENT',		SONIC_SDK__APP_NAME . '/' . SONIC_SDK__APP_VERSION);
