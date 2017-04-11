4eb57051398a414beb5cf4a3cc9617ed2ddf7824
Sebastian Göndör <sebastian.goendoer@tu-berlin.de>
2017-04-11 13:41:01 +0200

    corrected misleading variable names

--- src/Api/StreamRequestBuilder.php
+++ src/Api/StreamRequestBuilder.php
@@ -18,13 +18,13 @@ class StreamRequestBuilder extends AbstractRequestBuilder
 {
 	const RESOURCE_NAME_STREAM = 'STREAM';
 	
-	public function createGETStream($streamUOID = NULL)
+	public function createGETStream($streamItemUOID = NULL)
 	{
 		$this->request = new OutgoingRequest();
 		
 		$this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
-		if($streamUOID != NULL)
-			$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamUOID);
+		if($streamItemUOID != NULL)
+			$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamItemUOID);
 		else
 			$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM);
 		$this->request->setRequestMethod('GET');
@@ -32,34 +32,34 @@ class StreamRequestBuilder extends AbstractRequestBuilder
 		return $this;
 	}
 	
-	public function createGETStreamComment($commentUOID)
+	public function createGETStreamComment($streamItemUOID)
 	{
 		$this->request = new OutgoingRequest();
 		
 		$this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
-		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $commentUOID . '/' . CommentRequestBuilder::RESOURCE_NAME_COMMENT);
+		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamItemUOID . '/' . CommentRequestBuilder::RESOURCE_NAME_COMMENT);
 		$this->request->setRequestMethod('GET');
 		
 		return $this;
 	}
 	
-	public function createGETStreamLike($commentUOID)
+	public function createGETStreamLike($streamItemUOID)
 	{
 		$this->request = new OutgoingRequest();
 		
 		$this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
-		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $commentUOID . '/' . LikeRequestBuilder::RESOURCE_NAME_LIKE);
+		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamItemUOID . '/' . LikeRequestBuilder::RESOURCE_NAME_LIKE);
 		$this->request->setRequestMethod('GET');
 		
 		return $this;
 	}
 	
-	public function createGETStreamTag($commentUOID)
+	public function createGETStreamTag($streamItemUOID)
 	{
 		$this->request = new OutgoingRequest();
 		
 		$this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
-		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $commentUOID . '/' . TagRequestBuilder::RESOURCE_NAME_TAG);
+		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamItemUOID . '/' . TagRequestBuilder::RESOURCE_NAME_TAG);
 		$this->request->setRequestMethod('GET');
 		
 		return $this;
@@ -89,12 +89,12 @@ class StreamRequestBuilder extends AbstractRequestBuilder
 		return $this;
 	}
 	
-	public function createDELETEStream($streamUOID)
+	public function createDELETEStream($streamItemUOID)
 	{
 		$this->request = new OutgoingRequest();
 		
 		$this->request->setServer($this->getDomainFromProfileLocation($this->targetSocialRecord->getProfileLocation()));
-		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamUOID);
+		$this->request->setPath($this->getPathFromProfileLocation($this->targetSocialRecord->getProfileLocation()) . $this->targetSocialRecord->getGlobalID() . '/' . self::RESOURCE_NAME_STREAM . '/' . $streamItemUOID);
 		$this->request->setRequestMethod('DELETE');
 		
 		return $this;
